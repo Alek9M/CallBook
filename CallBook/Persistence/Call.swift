71faaf336491ade59838db3b19d11a2b92f7a957
M <malekuk9@gmail.com>
2023-11-29 19:44:58 +0000

    Created models

---
+++ CallBook/Persistence/Call.swift
@@ -0,0 +1,8 @@
+//
+//  Call.swift
+//  CallBook
+//
+//  Created by M on 29/11/2023.
+//
+
+import Foundation
---
+++ CallBook/Persistence/Callee.swift
@@ -0,0 +1,8 @@
+//
+//  Callee.swift
+//  CallBook
+//
+//  Created by M on 29/11/2023.
+//
+
+import Foundation
