f29e61b0dfcaf0de09a5278cd30752e526a31d9f
M <malekuk9@gmail.com>
2023-11-29 19:30:13 +0000

    Initial Commit

---
+++ CallBook/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CallBook
+//
+//  Created by M on 29/11/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
